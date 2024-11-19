7d6e3b50b24e5267e0c9d9239cab09345d1b6628
Aaron <157578322+AaronKuan0109@users.noreply.github.com>
2024-11-19 18:19:07 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -74,6 +74,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // 隱藏範例問題的容器
     exampleQuestionsContainer.style.display = "none";
+    mainTitle.style.display = "none";
+    topRightTitle.style.display = "block";
 
     // 顯示範例問題的功能
     function displayExampleQuestions(questions) {
